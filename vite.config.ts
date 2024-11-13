63f24006db9e8764819e6867f381a50d96e1ddf9
Trevor Miller <trevormil@comcast.net>
2024-11-13 09:36:06 -0500

    fix overlay?

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,12 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': {}
+    'process.env': {},
+  },
+  server: {
+    hmr: {
+      overlay: false,
+    },
   },
   plugins: [
     vue({
